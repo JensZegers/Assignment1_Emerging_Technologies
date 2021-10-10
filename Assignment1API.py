10aa02717270d1a5d1817e9897a8ac2ea6724db7
NetAcad DEVASC <changeme@example.com>
2021-10-10 15:44:35 +0000

    Comments toegevoegd

--- Assignment1API.py
+++ Assignment1API.py
@@ -1,11 +1,13 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-API_KEY = input("Geef uw API-KEY in:")
-request = input("Geef de naam van een film in: ")
-response = requests.get('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + request)
-JSONB = response.json() # store parsed json response
+API_KEY = input("Geef uw API-KEY in:") # Vraagt aan gebruiker voor de API-KEY
+request = input("Geef de naam van een film in: ") # Vraagt naar de naam van de film
+response = requests.get('https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=' + request) #Stuurt de request met de API-KEY en filmnaam toegevoegd
+JSONB = response.json() # json file in variabele steken
 
+
+# Vervolgens worden de gegevens die interessant zijn geselecteerd en uitgeprint
 print("=============================================")
 print('Titel: '+ JSONB['results'][0]['title']+'\n')
 print('Originele Taal: '+ JSONB['results'][0]['original_language']+'\n')
